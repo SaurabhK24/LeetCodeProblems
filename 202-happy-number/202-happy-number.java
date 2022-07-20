0afc183cba7a286bfc693a12687cb9bc88a452ad
Saurabh Khanal <Skhanal247@gmail.com>
2022-07-20 00:35:59 -0400

    Time: 3 ms (45.75%), Space: 41.3 MB (52.34%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -3,11 +3,11 @@ class Solution {
         
         HashSet<Integer> mySet = new HashSet<>();
         int remainder;
-        int currentSum;
+        int currentSum = 0;
         
         
         while(mySet.add(n)){
-            currentSum = 0;
+           currentSum = 0;
             while(n > 0){
                 remainder = n % 10;
                 currentSum = currentSum + (remainder * remainder);
